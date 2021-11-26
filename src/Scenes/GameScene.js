0e26df65b67ad9e13c477fd969d6893580629ed7
Kayla <kayla.marie.792@gmail.com>
2021-11-26 03:30:54 -0700

    several basic scenes working with logos

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -7,27 +7,11 @@ export default class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet("intro", "assets/intro.png", {
-      frameWidth: 128,
-      frameHeight: 128,
-      endFrame: 290,
-    });
+    this.load.image('logo', 'assets/logo.png');
   }
 
   create() {
-    var config = {
-      key: "introAnimation",
-      frames: this.anims.generateFrameNumbers("intro", {
-        start: 0,
-        end: 290,
-        first: 23,
-      }),
-      frameRate: 60,
-      repeat: -1,
-    };
-
-    this.anims.create(config);
-
-    this.add.sprite(800 / 2 , 600 / 2, "intro").play("introAnimation");
+    this.add.image(400, 300, 'logo');
   }
+
 }
